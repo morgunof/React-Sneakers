970d59605ae473b5ec571fcbe26fc0982c8614f3
morgunof <georgmorgunof@yandex.ru>
2023-01-28 02:27:53 +0300

    -props -styles modules -render(.map)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import Card from './components/Card'
+import Card from './components/Cards/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
+
+const arr = [
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 12999,
+        imageUrl: '/img/sneakers/1.svg'
+    },
+    {
+        title: 'Мужские Кроссовки Nike Air Max 270',
+        price: 13999,
+        imageUrl: '/img/sneakers/2.svg'
+    },
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 8499,
+        imageUrl: '/img/sneakers/3.svg'
+    },
+    {
+        title: 'Кроссовки Puma X Aka Boku Future Rider',
+        price: 8999,
+        imageUrl: '/img/sneakers/4.svg'
+    }
+]
 
 function App() {
   return (
@@ -18,10 +41,15 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+
+            {arr.map(obj => (
+                <Card
+                    title = {obj.title}
+                    price = {obj.price}
+                    imageUrl = {obj.imageUrl}
+                    onClick = {() => console.log(obj)}
+                />
+            ))}
         </div>
       </div>
     </div>
